8d273824a23cd59941c61fd74f191b3a42e5e471
kasinayana <kasinayana2@gmail.com>
2022-10-03 10:15:06 +0530

    Create,read and Update completed

---
+++ EnquiryProject/Controllers/EnquiryController.cs
@@ -0,0 +1,46 @@
+﻿using EnquiryProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnquiryProject.Controllers
+{
+    public class EnquiryController : Controller
+    {
+        EnquiryDb enDb = new EnquiryDb();
+        public ActionResult Index()
+        {
+            List<Enquiry> lstEnquiry = new List<Enquiry>();
+            lstEnquiry = enDb.ListAll();
+            return View(lstEnquiry);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.genderddl = enDb.GetGenderDropDown();
+            return View();
+        }
+        public ActionResult Add(Enquiry en)
+        {
+            enDb.Add(en);
+            List<Enquiry> lstEnquiry = new List<Enquiry>();
+            lstEnquiry = enDb.ListAll();
+            return View("Index", lstEnquiry);
+        }
+        public ActionResult Edit(int id)
+        {
+            
+            Enquiry en = new Enquiry();
+            en = enDb.GetEnquiryById(id);
+            return View(en);
+        }
+        public ActionResult Update(Enquiry en)
+        {
+            enDb.Update(en);
+            List<Enquiry> lstEnquiry = new List<Enquiry>();
+            lstEnquiry = enDb.ListAll();
+            return View("Index", lstEnquiry);
+        }
+    }
+}
---
+++ EnquiryProject/Models/Enquiry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EnquiryProject.Models
+{
+    public class Enquiry
+    {
+        public int EnquiryId { get; set; }
+        //public object EnquiryID { get; set; }
+        public int GenderId { get; set; }
+        public int CompanyId { get; set; }
+        public int SiteId { get; set; }
+        public int DepartmentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string Reference { get; set; }
+        public string HomeAddress { get; set; }
+        public string Tag { get; set; }
+        public string Comments { get; set; }
+        public bool DisplayOnWeb { get; set; }
+        public int SortOrder { get; set; }
+        public string IpAddress { get; set; }
+        public bool IsPublished { get; set; }
+        public string PublishedBy { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string UpDateBy { get; set; }
+        public string LastUpdate { get; set; }
+        public bool IsDeleted { get; set; }
+        public string DeletedBy { get; set; }
+        public DateTime DateDeleted { get; set; }
+
+        //public class Models
+        //{
+        //}
+
+        //public override bool Equals(object obj)
+        //{
+        //    return obj is Enquiry enquiry &&
+        //           EqualityComparer<object>.Default.Equals(EnquiryID, enquiry.EnquiryID);
+        //}
+    }
+}
